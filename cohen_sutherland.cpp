7d114354e0063e35293de303399940f8720397dd
Shreya Sriram <76148126+shreya1110-dev@users.noreply.github.com>
2022-12-04 20:49:26 +0530

    Update cohen_sutherland.cpp

--- cohen_sutherland.cpp
+++ cohen_sutherland.cpp
@@ -215,7 +215,7 @@ int main(int argc, char* argv[]) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(1000, 1000);
-	glutCreateWindow("Window to Viewport Mapping");
+	glutCreateWindow("Cohen Sutherland");
 	glutDisplayFunc(display);
 	init();
 	glutMainLoop();
